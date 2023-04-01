f89ba80b0f918b9b95aa93955f2b01485efe1670
Tomasz Mazur <mazur.tomasz34@gmail.com>
2023-04-01 23:53:04 +0200

    Better fallback

--- src/routing/mod.rs
+++ src/routing/mod.rs
@@ -18,20 +18,24 @@ pub fn get_main_router(pool: &PgPool) -> Router {
     let authorized_handler = get(|user: User| async move { format!("Hello, {}", user.email) })
         .layer(auth);
     
-    let router = Router::new()
+    
+
+    let cors = match env::var(constants::CLIENT_URL_ENV_VAR) {
+        Ok(client_url) => CorsLayer::new()
+            .allow_origin(client_url.parse::<HeaderValue>().unwrap())
+            .allow_headers(Any),
+        Err(_) => CorsLayer::new()
+            .allow_origin(Any)
+            .allow_headers(Any)
+    };
+
+    Router::new()
         .nest("/users", users_router(pool))
         .nest("/sessions", sessions_router(pool))
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/authorized", authorized_handler);
+        .route("/authorized", authorized_handler)
+        .layer(cors)
 
     // setup CORS, if possible
-    if let Ok(client_url) = env::var(constants::CLIENT_URL_ENV_VAR) {
-        let cors = CorsLayer::new()
-            .allow_origin(client_url.parse::<HeaderValue>().unwrap())
-            .allow_headers(Any);
-
-        router.layer(cors)
-    } else {
-        router
-    }
+    
 }
